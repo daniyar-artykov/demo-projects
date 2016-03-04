9d61106e5d028600c6456062ffb36abde96b5373
Daniyar Artykov <daniyar.artykov@gmail.com>
2016-03-04 09:41:54 +0600

    tabbed pane added

--- ftp/src/ClientForm.java
+++ ftp/src/ClientForm.java
@@ -2,9 +2,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -15,92 +17,160 @@ public class ClientForm extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -2442287481161446544L;
-	private JPanel panel;
+	private JPanel topPanel;
 	private JLabel serverAddressLabel;
 	private JTextArea serverAddress;
+
+	private JTabbedPane tabbedPane;
+	private JPanel uploadPanel;
+	private JPanel downloadPanel;
+
 	private JButton upload;
 	private JButton download;
-	private JLabel msg;
-	private JLabel sourceLabel;
-	private JTextField source;
-	private JLabel destinationLabel;
-	private JTextField destination;
 
+	private JLabel msg;
+
+	private JLabel uploadSourceLabel;
+	private JTextField uploadSource;
+	private JButton uploadSourceOpen;
+	private JLabel uploadDestinationLabel;
+	private JTextField uploadDestination;
+
+	private JLabel downloadSourceLabel;
+	private JTextField downloadSource;
+	private JLabel downloadDestinationLabel;
+	private JTextField downloadDestination;
 
 	public ClientForm() {
 		// set default size of the client form
-		this.setSize(350, 300);
+		this.setSize(457, 320);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		// set title for client form
 		this.setTitle("Client");
 
 		// add main panel to client form
-		panel = new JPanel();
-		panel.setLayout(null);
+		topPanel = new JPanel();
+		topPanel.setLayout(null);
 
-		this.add(panel);
+		this.add(topPanel);
 
 		serverAddressLabel = new JLabel("Server");
-		serverAddressLabel.setBounds(7, 5, 90, 30);
-		panel.add(serverAddressLabel);
+		serverAddressLabel.setBounds(12, 5, 90, 30);
+		topPanel.add(serverAddressLabel);
 
 		// server address input, default is "127.0.0.1:23111"
 		serverAddress = new JTextArea("127.0.0.1:23111");
 		serverAddress.setBounds(105, 5, 150, 30);
-		panel.add(serverAddress);
+		topPanel.add(serverAddress);
 
-		sourceLabel = new JLabel("Source");
-		sourceLabel.setBounds(5, 40, 80, 30);
-		panel.add(sourceLabel);
+		// Create a tabbed pane
+		tabbedPane = new JTabbedPane();
 
-		source = new JTextField("server.txt");
-		source.setBounds(105, 40, 150, 30);
-		panel.add(source);
+		uploadPanel = new JPanel();
+		uploadPanel.setLayout(null);
 
-		destinationLabel = new JLabel("Destination");
-		destinationLabel.setBounds(5, 80, 80, 30);
-		panel.add(destinationLabel);
+		downloadPanel = new JPanel();
+		downloadPanel.setLayout(null);
 
-		destination = new JTextField("from-server.txt");
-		destination.setBounds(105, 80, 150, 30);
-		panel.add(destination);
+		tabbedPane.addTab("Upload", uploadPanel);
+		tabbedPane.addTab("Download", downloadPanel);
+
+		tabbedPane.setBounds(5, 40, 450, 160);
+
+		topPanel.add(tabbedPane);
+
+		uploadSourceLabel = new JLabel("Source");
+		uploadSourceLabel.setBounds(5, 5, 80, 30);
+		uploadPanel.add(uploadSourceLabel);
+
+		uploadSource = new JTextField();
+		uploadSource.setEnabled(false);
+		uploadSource.setBounds(105, 5, 200, 30);
+		uploadPanel.add(uploadSource);
+
+		uploadSourceOpen = new JButton("Browse...");
+		uploadSourceOpen.setBounds(310, 5, 100, 30);
+		uploadPanel.add(uploadSourceOpen);
+
+		uploadDestinationLabel = new JLabel("Destination");
+		uploadDestinationLabel.setBounds(5, 40, 80, 30);
+		uploadPanel.add(uploadDestinationLabel);
+
+		uploadDestination = new JTextField("from-client.txt");
+		uploadDestination.setBounds(105, 40, 150, 30);
+		uploadPanel.add(uploadDestination);
 
 		// upload button
 		upload = new JButton("Upload");
-		upload.setBounds(5, 120, 95, 30);
-		panel.add(upload);
+		upload.setBounds(5, 80, 120, 30);
+		uploadPanel.add(upload);
+
+		downloadSourceLabel = new JLabel("Source");
+		downloadSourceLabel.setBounds(5, 5, 80, 30);
+		downloadPanel.add(downloadSourceLabel);
+
+		downloadSource = new JTextField("server.txt");
+		downloadSource.setBounds(105, 5, 150, 30);
+		downloadPanel.add(downloadSource);
+
+		downloadDestinationLabel = new JLabel("Destination");
+		downloadDestinationLabel.setBounds(5, 40, 80, 30);
+		downloadPanel.add(downloadDestinationLabel);
+
+		downloadDestination = new JTextField("from-server.txt");
+		downloadDestination.setBounds(105, 40, 150, 30);
+		downloadPanel.add(downloadDestination);
 
 		// download button
 		download = new JButton("Download");
-		download.setBounds(105, 120, 95, 30);
-		panel.add(download);
+		download.setBounds(5, 80, 120, 30);
+		downloadPanel.add(download);
 
 		msg = new JLabel();
-		msg.setBounds(5, 160, 280, 30);
-		panel.add(msg);
+		msg.setBounds(5, 200, 280, 30);
+		topPanel.add(msg);
+
+		uploadSourceOpen.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser c = new JFileChooser();
+				int rVal = c.showOpenDialog(ClientForm.this);
+				if (rVal == JFileChooser.APPROVE_OPTION) {
+					uploadSource.setText(c.getCurrentDirectory().toString() + "/" + c.getSelectedFile().getName());
+				}
+				if (rVal == JFileChooser.CANCEL_OPTION) {
+					uploadSource.setText("");
+				}
+			}
+		});
 
 		upload.addActionListener(new ActionListener() { 
+
+			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(validateFields()) {
+				if(validateUploadFields()) {
 					String[] address = serverAddress.getText().split(":");
-					String rsp = FileClient.upload(address[0], Integer.parseInt(address[1]), source.getText(), destination.getText());
+					String rsp = FileClient.upload(address[0], Integer.parseInt(address[1]), uploadSource.getText(), uploadDestination.getText());
 					msg.setText(rsp);
 				}
 			}
 		});
 
 		download.addActionListener(new ActionListener() { 
+
+			@Override
 			public void actionPerformed(ActionEvent e) { 
-				if(validateFields()) {
+				if(validateDownloadFields()) {
 					String[] address = serverAddress.getText().split(":");
-					String rsp = FileClient.download(address[0], Integer.parseInt(address[1]), source.getText(), destination.getText());
+					String rsp = FileClient.download(address[0], Integer.parseInt(address[1]), downloadSource.getText(), downloadDestination.getText());
 					msg.setText(rsp);
 				}
 			}
 		});
 	}
 
-	public boolean validateFields() {
+	public boolean validateUploadFields() {
 		if(serverAddress.getText() == null 
 				|| "".equals(serverAddress.getText())) {
 			msg.setText("Error: The File address is wrong! Format ip:port!");
@@ -112,15 +182,43 @@ public class ClientForm extends JFrame {
 				return false;
 			}
 		}
-		
-		if(source.getText() == null 
-				|| "".equals(source.getText())) {
+
+		if(uploadSource.getText() == null 
+				|| "".equals(uploadSource.getText())) {
 			msg.setText("Error: Source file isn't set!");
 			return false;
 		}
-		
-		if(destination.getText() == null 
-				|| "".equals(destination.getText())) {
+
+		if(uploadDestination.getText() == null 
+				|| "".equals(uploadDestination.getText())) {
+			msg.setText("Error: Destination file isn't set!");
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean validateDownloadFields() {
+		if(serverAddress.getText() == null 
+				|| "".equals(serverAddress.getText())) {
+			msg.setText("Error: The File address is wrong! Format ip:port!");
+			return false;
+		} else {
+			String[] address = serverAddress.getText().split(":");
+			if(address == null || address.length != 2) {
+				msg.setText("Error: The File address is wrong! Format ip:port!");
+				return false;
+			}
+		}
+
+		if(downloadSource.getText() == null 
+				|| "".equals(downloadSource.getText())) {
+			msg.setText("Error: Source file isn't set!");
+			return false;
+		}
+
+		if(downloadDestination.getText() == null 
+				|| "".equals(downloadDestination.getText())) {
 			msg.setText("Error: Destination file isn't set!");
 			return false;
 		}
